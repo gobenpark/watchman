9df96cf2d8b392359c3e8665047ec22ca04029c2
ben <qjadn0914@naver.com>
2024-06-09 22:16:22 +0900

    init

---
+++ src/storage/models.rs
@@ -0,0 +1,13 @@
+use diesel::data_types::PgTimestamp;
+use diesel::prelude::*;
+use diesel::sql_types::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::orders)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Order {
+    pub id: String,
+    pub price: Option<f64>,
+    pub amount: Option<f64>,
+    pub created_at: PgTimestamp,
+}
